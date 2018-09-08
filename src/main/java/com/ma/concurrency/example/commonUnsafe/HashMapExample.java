2ce037f6c4fb991a6d0bea4249eb6af4c274a32b
YueMa233 <mayueluck@dingtalk.com>
2018-09-08 20:32:27 +0800

    【线程不安全的类的测试】

---
+++ src/main/java/com/ma/concurrency/example/commonUnsafe/HashMapExample.java
@@ -0,0 +1,48 @@
+package com.ma.concurrency.example.commonUnsafe;
+
+import com.ma.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class HashMapExample {
+
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static HashMap<Integer,Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for(int i = 0;i < clientTotal ; i++){
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("Size:{}",map.size());
+    }
+    public static void add(int i){
+        map.put(i,i);
+    }
+}
